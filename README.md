360f880354d7cce86a408703fcb9317ea18665e1
mokadde <mokaddes.ru2000@gmail.com>
2024-10-24 22:36:28 +0600

    commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Javascript Intermediate1 With Anisul Islam Youtube Live Class
-## String/Tempelate Literals
-## var, Let vs Const
-## Module
-## Spread & Rest Parameter
-## Destructuring
-## default value
+## 1.String/Tempelate Literals
+## 2.var, Let vs Const
+## 3.Module
+## 4.Spread & Rest Parameter
+## 5.Destructuring
+## 6.default value
--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let fullName = 'John Doe';
-let age = 25;
+// //Srting/Tempelate Literate
 
-console.log(fullName);
-console.log(age);
+let fullName = 'Mokaddes Ali';
+
+console.log(`My name is ${fullName}`);  //`` it's called backtick
+
+
+// var not use now as a variable
+
+//let and const are used now as a variable
+//but let is changeable and const is not changeable
+
+ let fullName1 = 'Mokaddes Ali';
+
+ fullName = 'Ali';
+
+    console.log(fullName1); //let call last value
+
+
+
+
+
+
